e6b9b3c8617e6bed993505fd06d3d5a634400c93
CelestePapalato <50720541+CelestePapalato@users.noreply.github.com>
2024-04-08 17:10:48 -0300

    Corregido verificación de LayerMask en PlayerEvent. Añadido modelo 3D al player.

--- Assets/Player/Scripts/PlayerEvent.cs
+++ Assets/Player/Scripts/PlayerEvent.cs
@@ -5,12 +5,18 @@ public class PlayerEvent : MonoBehaviour
 {
     [SerializeField]
     LayerMask deathZoneLayerMask;
+    int deathZoneLayerMaskValue;
 
 
     public UnityAction GameOver;
+
+    private void Awake()
+    {
+        deathZoneLayerMaskValue = (int) Mathf.Log(deathZoneLayerMask.value, 2);
+    }
     private void OnTriggerEnter(Collider collider)
     {
-        if(collider.gameObject.layer != deathZoneLayerMask)
+        if(collider.gameObject.layer != deathZoneLayerMaskValue)
         {
             return;
         }
