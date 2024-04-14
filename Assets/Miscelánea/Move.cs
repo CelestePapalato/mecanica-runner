d2c385169fa8a8dd1eada6ba2ec044abb3b40f5e
CelestePapalato <50720541+CelestePapalato@users.noreply.github.com>
2024-04-14 15:49:28 -0300

    Move ahora tiene una variable que añade velocidad por encima del SueloManager al objeto

--- Assets/Miscelánea/Move.cs
+++ Assets/Miscelánea/Move.cs
@@ -2,8 +2,9 @@ using UnityEngine;
 
 public class Move : MonoBehaviour
 {
+    [SerializeField] float addedSpeed;
     void FixedUpdate()
     {
-        transform.Translate(SueloManager.DireccionDeMovimiento * SueloManager.Velocidad * Time.deltaTime * GameManager.VelocidadDeJuego);
+        transform.Translate(SueloManager.DireccionDeMovimiento * (SueloManager.Velocidad + addedSpeed) * Time.deltaTime * GameManager.VelocidadDeJuego);
     }
 }
