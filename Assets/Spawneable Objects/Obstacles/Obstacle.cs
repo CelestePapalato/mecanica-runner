7d4e939e287db4dae51e436d47387b707c08a654
CelestePapalato <50720541+CelestePapalato@users.noreply.github.com>
2024-04-16 16:52:28 -0300

    Los Obstáculos ahora se destruyen si no están sobre suelo. Modificada altura de power ups en Obstacle 2

--- Assets/Spawneable Objects/Obstacles/Obstacle.cs
+++ Assets/Spawneable Objects/Obstacles/Obstacle.cs
@@ -4,15 +4,35 @@ using UnityEngine;
 
 public class Obstacle : MonoBehaviour
 {
-    NonVisible nonVisible;
+    [SerializeField] LayerMask SueloLayerMask;
+    [SerializeField] float distanciaRaycast;
+    NonVisible _nonVisible;
     void Awake()
     {
-        nonVisible = GetComponent<NonVisible>();
-        nonVisible.action += destroy;
+        _nonVisible = GetComponent<NonVisible>();
+        _nonVisible.action += destroy;
+    }
+
+    private void Update()
+    {
+        Vector3 position = transform.position;
+        Vector3 scale = new Vector3(0, 0, transform.localScale.z) * 1/2;
+        bool center = Physics.Raycast(position, Vector3.down, distanciaRaycast, SueloLayerMask);
+        bool left = Physics.Raycast(position + scale, Vector3.down, distanciaRaycast, SueloLayerMask);
+        bool right = Physics.Raycast(position - scale, Vector3.down, distanciaRaycast, SueloLayerMask);
+        if(!(center || left || right))
+        {
+            Destroy(gameObject);
+        }
     }
 
     private void destroy()
     {
         Destroy(gameObject);
+    }
+
+    private void OnDestroy()
+    {
+        _nonVisible.action -= destroy;
     }
 }
